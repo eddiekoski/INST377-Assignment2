580ca631e9628bada399e0885c15cd3c6383fc01
Robert Y <r.harrison.y@gmail.com>
2020-04-02 22:39:40 -0400

    light/dark button

--- Assignment2/main.js
+++ Assignment2/main.js
@@ -4,10 +4,26 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Looping through images */
+/* Looping through images 
 
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
+newImage.setAttribute('src');
 thumbBar.appendChild(newImage);
 
+
+
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function(){
+    const btnClass = btn.getAttribute('class');
+    if(btnClass === 'dark'){
+        btn.setAttribute('class', 'light');
+        btn.textContent= 'Lighten';
+        overlay.style.backgroundColor='rgba(0,0,0,0.5)';
+
+    } else{
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor= 'rgba(0,0,0,0)';
+    
+    }
+}
